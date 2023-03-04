8c1f02dc89f16a02806fa5655e13651d667772b1
Michael Vo <zax@zaxu.xyz>
2023-03-04 13:52:27 +1100

    feat: glow

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "twin.macro";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import BackgroundGlow from "components/BackgroundGlow";
 import NavBar from "components/Navbar";
 import Landing from "pages/landing";
 import ProductPage from "pages/product";
@@ -15,6 +16,7 @@ const App = () => (
   <div tw="flex flex-col bg-gray-100 h-screen">
     <BrowserRouter>
       <main tw="flex flex-col gap-4 flex-1 p-9">
+        <BackgroundGlow />
         <Routes>{routes}</Routes>
       </main>
       <NavBar />
