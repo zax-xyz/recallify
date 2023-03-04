4e1e1b9a68704d69859ee66a4077e36e9325de80
Jared L <48422312+lhjt@users.noreply.github.com>
2023-03-04 23:55:57 +1100

    feat(backend): init prisma

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,1 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
 
+const prisma = new PrismaClient();
+const app = express();
+
+async function main() {
+  app.listen(3001, "0.0.0.0", () => {
+    console.log("Listening on port 3001");
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
