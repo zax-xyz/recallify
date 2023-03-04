50c19015b2215ba6c6799d20181d86786a7b33e4
Michael Vo <zax@zaxu.xyz>
2023-03-04 12:38:17 +1100

    feat(navbar): make buttons clickable
    skull emoji

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import "twin.macro";
 
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import NavBar from "components/Navbar";
 import Landing from "pages/landing";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Landing />,
-  },
-]);
+const routes = [<Route path="/" element={<Landing />} />];
 
 const App = () => (
   <div tw="flex flex-col bg-gray-100 h-screen">
-    <main tw="flex flex-col gap-4 flex-1 p-9">
-      <RouterProvider router={router} />
-    </main>
-    <NavBar />
+    <BrowserRouter>
+      <main tw="flex flex-col gap-4 flex-1 p-9">
+        <Routes>{routes}</Routes>
+      </main>
+      <NavBar />
+    </BrowserRouter>
   </div>
 );
 
