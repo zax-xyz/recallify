2f1a93ee4b94f80b5a4fde4c8057aa3ce56627f2
Jared L <48422312+lhjt@users.noreply.github.com>
2023-03-05 00:45:19 +1100

    wip(backend): add basic user handler

---
+++ packages/backend/src/managers/user.ts
@@ -0,0 +1,32 @@
+import { TRPCError } from "@trpc/server";
+import argon2 from "argon2";
+import { z } from "zod";
+
+import { prisma } from "./prisma";
+import { procBuilder } from "./rpc";
+
+export const registerUserHandler = procBuilder
+  .input(
+    z.object({ email: z.string().email(), password: z.string().min(8), name: z.string().min(1) }),
+  )
+  .mutation(async ({ input }) => {
+    // Create a new user
+    const user = await prisma.user
+      .create({
+        data: {
+          email: input.email,
+          password: await argon2.hash(input.password),
+          name: input.name,
+        },
+      })
+      .catch((reason) => {
+        console.error("failed to create user:", reason);
+        throw new TRPCError({ code: "BAD_REQUEST", message: reason });
+      });
+
+    return {
+      email: user.email,
+      name: user.name,
+      id: user.id,
+    };
+  });
