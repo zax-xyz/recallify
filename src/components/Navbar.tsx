4263d86b8a9b4d040d0d5f331f0342b7f0454a2d
Michael Vo <zax@zaxu.xyz>
2023-03-04 13:55:58 +1100

    feat(navbar): elevation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ const Tab = styled(NavLink, {
 });
 
 const NavBar = () => (
-  <nav tw="fixed bottom-0 inset-x-0 bg-white py-2 px-4">
+  <nav tw="fixed bottom-0 inset-x-0 bg-white py-2 px-4 shadow-2">
     <ul tw="flex justify-around">
       {tabs.map(({ to, icon, name }) => (
         <li>
