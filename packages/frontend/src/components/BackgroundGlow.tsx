db4a0c17e3bda8f11ee10c2239717aba513a6174
Michael Vo <zax@zaxu.xyz>
2023-03-05 15:51:37 +1100

    feat: increase background glow visibility

--- packages/frontend/src/components/BackgroundGlow.tsx
+++ packages/frontend/src/components/BackgroundGlow.tsx
@@ -4,9 +4,9 @@ const Glow = tw.div`absolute rounded-full`;
 
 const BackgroundGlow = () => (
   <div tw="fixed inset-0 z-[-1]">
-    <Glow tw="w-[200px] h-[200px] top-[53%] left-[15%] bg-[#78a3e2]/[0.26] blur-[200px]" />
-    <Glow tw="w-[172px] h-[172px] top-[47%] left-1/2 bg-[#8778e2]/[0.12] blur-[172px]" />
-    <Glow tw="w-[156px] h-[156px] top-[36%] left-[13%] bg-[#d7b255]/[0.09] blur-[132px]" />
+    <Glow tw="w-[200px] h-[200px] top-[53%] left-[15%] bg-[#78a3e2]/[0.3] blur-[70px]" />
+    <Glow tw="w-[172px] h-[172px] top-[47%] left-1/2 bg-[#8778e2]/[0.15] blur-[60px]" />
+    <Glow tw="w-[156px] h-[156px] top-[35%] left-1/4 bg-[#d7b255]/[0.1] blur-[50px]" />
   </div>
 );
 
