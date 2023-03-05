c644e5bcab547a20843e6f62b6316c6f88f1436b
Michael Vo <zax@zaxu.xyz>
2023-03-05 12:51:58 +1100

    fix(SearchResults): prop spread destructuring

--- packages/frontend/src/pages/landing/SearchResults.tsx
+++ packages/frontend/src/pages/landing/SearchResults.tsx
@@ -11,38 +11,32 @@ type SearchResultsProps = {
   results: any[];
 };
 
-const SearchResults = ({...props, results}: ComponentProps<"div"> & SearchResultsProps) => {
-  return (
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <div tw="flex flex-col gap-2" {...props}>
-      <h2>Results for &quot;Fruits&quot;</h2>
-      {
-        results.length === 0 && (
-          <p tw="text-center">No results found</p>
-        )
-      }
-      <ul tw="flex flex-col gap-2">
-        <li>
-          <Link to="/product/1">
-            <Card tw="flex gap-8 items-center">
-              <img tw="w-28" src={banana} alt="banana" />
-              <div tw="flex flex-col gap-2">
-                <div>
-                  <h3 tw="text-base font-normal my-0">Banana Cavendish</h3>
-                  <p tw="text-sm text-light-neutral-700">Woolworths</p>
-                </div>
-
-                <div>
-                  <h3 tw="text-base font-normal my-0">Recalled on</h3>
-                  <p tw="text-sm text-light-neutral-700">14th February 2023</p>
-                </div>
+const SearchResults = ({ results, ...props }: ComponentProps<"div"> & SearchResultsProps) => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <div tw="flex flex-col gap-2" {...props}>
+    <h2>Results for &quot;Fruits&quot;</h2>
+    {results.length === 0 && <p tw="text-center">No results found</p>}
+    <ul tw="flex flex-col gap-2">
+      <li>
+        <Link to="/product/1">
+          <Card tw="flex gap-8 items-center">
+            <img tw="w-28" src={banana} alt="banana" />
+            <div tw="flex flex-col gap-2">
+              <div>
+                <h3 tw="text-base font-normal my-0">Banana Cavendish</h3>
+                <p tw="text-sm text-light-neutral-700">Woolworths</p>
               </div>
-            </Card>
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
-};
+
+              <div>
+                <h3 tw="text-base font-normal my-0">Recalled on</h3>
+                <p tw="text-sm text-light-neutral-700">14th February 2023</p>
+              </div>
+            </div>
+          </Card>
+        </Link>
+      </li>
+    </ul>
+  </div>
+);
 
 export default SearchResults;
